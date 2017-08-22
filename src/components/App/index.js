868bc695fd978e9a4ed1f820dfa0c56847febf48
Javi <jjasso1@asu.edu>
2017-08-22 10:28:25 -0400

    final commit

---
+++ src/components/App/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import BaseLayout from '../BaseLayout';
+import ParentComponent from '../ParentComponent';
+
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BaseLayout>
+
+          <ParentComponent />
+
+        </BaseLayout>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ParentComponent/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import DisplayComponent from '../DisplayComponent';
+import ChildComponent from '../ChildComponent';
+
+
+class ParentComponent extends Component {
+  constructor(props){
+    super(props);
+    this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    //state lives here
+    this.state = {
+      whatToSay: "",
+      whatWasSaid: "",
+    }
+  }
+  handleInput(e) {
+    e.preventDefault();
+    //set the state on input change
+    this.setState({whatToSay: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    //check console to see if firing
+    console.log("fired");
+    //set the state for props and for value (prevents output from appearing when typing)
+    this.setState({whatToSay: this.state.whatToSay, whatWasSaid: this.state.whatToSay});
+    //clear our input by resetting state
+    this.setState({whatToSay: ""});
+
+  }
+  render() {
+    return (
+      <div>Smart Component: I have a function, but something isn't working? I also need to pass that function to the ChildComponent.
+        <div>
+          <input onChange={this.handleInput} type="text" placeholder="Say It, Don't Spray It!" value={this.state.whatToSay} />
+        </div>
+        <div>
+          <ChildComponent onClick={this.handleSubmit}/>
+          <DisplayComponent sayWhat={this.state.whatWasSaid} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ParentComponent
+
+//The ParentComponent has a lot of the functionality worked out for you, but you will need to make it run properly. Namely in the constructor method and the render method where we pass props down.
+//
